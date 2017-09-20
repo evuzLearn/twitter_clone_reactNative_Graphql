17cc9567b776e059f8e4c50b3e3ce7b4d0f82ced
evuz <jesus_jgb_45@hotmail.com>
2017-09-20 10:23:31 +0200

    Server: Create User model

--- server/src/mocks/index.js
+++ server/src/mocks/index.js
@@ -1,14 +1,18 @@
 import faker from 'faker';
 import Tweet from '../models/Tweet';
+import User from '../models/User';
 
 const TWEETS_TOTAL = 10;
 
 export default async () => {
   try {
-    await Tweet.remove();
-    await Array.from({ length: TWEETS_TOTAL }).forEach(
-      async () => await Tweet.create({ text: faker.lorem.paragraphs(1) }),
-    );
+    if (process.env.NODE_ENV === 'dev') {
+      await Tweet.remove();
+      await User.remove();
+      await Array.from({ length: TWEETS_TOTAL }).forEach(
+        async () => await Tweet.create({ text: faker.lorem.paragraphs(1) }),
+      );
+    }
   } catch (err) {
     throw err;
   }
---
+++ server/src/models/User.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+    },
+    firstName: String,
+    lastName: String,
+    avatar: String,
+    password: String,
+    email: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model('User', UserSchema);
