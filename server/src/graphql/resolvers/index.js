c28dee3236c3849b5b85fc2db688dc985447f008
evuz <jesus_jgb_45@hotmail.com>
2017-09-20 10:23:53 +0200

    Server: mutation signup

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,16 +1,18 @@
 import GraphQLDate from 'graphql-date';
 
 import TweetResolvers from './tweet-resolvers';
+import UserResolvers from './user-resolvers';
 
 export default {
   Date: GraphQLDate,
   Query: {
     getTweets: TweetResolvers.getTweets,
-    getTweet: TweetResolvers.getTweet
+    getTweet: TweetResolvers.getTweet,
   },
   Mutation: {
     createTweet: TweetResolvers.createTweet,
     updateTweet: TweetResolvers.updateTweet,
-    deleteTweet: TweetResolvers.deleteTweet
-  }
+    deleteTweet: TweetResolvers.deleteTweet,
+    signup: UserResolvers.signup,
+  },
 };
--- server/src/graphql/schema.js
+++ server/src/graphql/schema.js
@@ -8,6 +8,17 @@ export default `
     updateAt: Date!
   }
 
+  type User {
+    _id: ID!
+    username: String
+    email: String!
+    firstName: String
+    lastName: String
+    avatar: String
+    createdAt: Date!
+    updatedAt: Date!
+  }
+
   type Status {
     message: String!
   }
@@ -20,7 +31,8 @@ export default `
   type Mutation {
     createTweet(text: String!): Tweet
     updateTweet(_id: ID!, text: String): Tweet
-    deleteTweet(_id: ID): Status
+    deleteTweet(_id: ID): Status,
+    signup(email: String!, fullName: String!, password: String!, avatar: String, username: String): User
   }
 
   schema {
