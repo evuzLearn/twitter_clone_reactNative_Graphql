1aa1ded149f9628a9c1a6a35e1c4a1d7a0133dd7
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-09-25 00:01:55 +0200

    Create component FeedCardBottom (template)

---
+++ mobile/src/components/Feed/FeedCardBottom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+// prettier-ignore
+const Root = styled.View`
+  height: 40;
+  backgroundColor: pink;
+`;
+
+const FeedCardBottom = () => <Root />;
+
+export default FeedCardBottom;
