0b6d25235a36e344bc17ba4b52de7cab498c264b
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-09-25 23:40:04 +0200

    Create FeedCardBottom

--- mobile/src/components/Feed/FeedCardBottom.js
+++ mobile/src/components/Feed/FeedCardBottom.js
@@ -1,12 +1,55 @@
 import React from 'react';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Touchable from '@appandflow/touchable';
 import styled from 'styled-components/native';
+
+import { colors } from '../../utils/constants';
+
+const ICON_SIZE = 20;
 
 // prettier-ignore
 const Root = styled.View`
   height: 40;
-  backgroundColor: pink;
+  flexDirection: row;
 `;
 
-const FeedCardBottom = () => <Root />;
+// prettier-ignore
+const Button = styled(Touchable).attrs({
+  feedback: 'opacity'
+})`
+  flex: 1;
+  flexDirection: row;
+  alignItems: center;
+  justifyContent: space-around;
+  paddingHorizontal: 32px;
+`;
+
+// prettier-ignore
+const ButtonText = styled.Text`
+  fontSize: 14;
+  fontWeight: 500;
+  color: ${props => props.theme.LIGHT_GRAY};
+`;
+
+const favoriteCount = 3;
+const isFavorite = false;
+
+const FeedCardBottom = () => (
+  <Root>
+    <Button>
+      <SimpleLineIcons name="bubbles" size={ICON_SIZE} color={colors.LIGHT_GRAY} />
+      <ButtonText>{ favoriteCount }</ButtonText>
+    </Button>
+    <Button>
+      <Entypo name="retweet" size={ICON_SIZE} color={colors.LIGHT_GRAY} />
+      <ButtonText>{ favoriteCount }</ButtonText>
+    </Button>
+    <Button>
+      <Entypo name="heart" size={ICON_SIZE} color={isFavorite ? 'red' : colors.LIGHT_GRAY} />
+      <ButtonText>{ favoriteCount }</ButtonText>
+    </Button>
+  </Root>
+);
 
 export default FeedCardBottom;
